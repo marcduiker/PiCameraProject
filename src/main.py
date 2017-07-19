125bba5a8710059a0e8dd1ff0eb77e1aea14ecfa
Marc Duiker <marcduiker@gmail.com>
2017-07-20 00:38:51 +0200

    Increment frame number after capture.

--- src/main.py
+++ src/main.py
@@ -7,10 +7,19 @@ from imagecleaner import ImageCleaner
 image_base_path = '/home/msd/Pictures/PiCam/'
 total_frames = 5
 current_frame = 1
+sleep_time = 5
 while current_frame < total_frames:
     camera_settings = CameraSettings()
     # Change default camera settings here if required
+    camera_settings.isautomatic = False
+    camera_settings.exposuremode = 'off'
+    camera_settings.framerate = 6
+    camera_settings.iso = 800
+    camera_settings.shutterspeed = 4 # seconds
+
     image_path = ImageRecorder(camera_settings, image_base_path).capture_image()
     ImageUploader().store_image(image_path)
     ImageCleaner().delete_local_image(image_path)
-    time.sleep(5)
+    
+    time.sleep(sleep_time)
+    current_frame += 1
