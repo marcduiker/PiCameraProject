4011e46c439d2f0e1484c307275802f12f25413e
Marc Duiker <marcduiker@gmail.com>
2017-07-20 12:35:02 +0200

    Reduced stabilisation time

--- src/imagerecorder.py
+++ src/imagerecorder.py
@@ -3,6 +3,8 @@ import time
 import picamera
 from camerasettings import CameraSettings
 
+STABILIZATION_TIME = 5
+
 class ImageRecorder(object):
     """ Class which controls the image recording of the Pi camera module. """
     def __init__(self, camerasettings, image_base_path):
@@ -11,7 +13,6 @@ class ImageRecorder(object):
 
     def capture_image(self):
         """ Method to capture an image using the CameraSettings class. """
-        sleeptime = 8
 
         with picamera.PiCamera() as camera:
             camera.resolution = (1640, 1232)
@@ -20,7 +21,7 @@ class ImageRecorder(object):
                 camera.iso = self.__camerasettings.iso
                 camera.framerate = self.__camerasettings.framerate
                 camera.shutter_speed = self.__camerasettings.shutterspeed
-            time.sleep(sleeptime)
+            time.sleep(STABILIZATION_TIME)
             filename = '{}.jpg'.format(time.strftime("%Y%m%d-%H%M%S"))
             filepath = os.path.join(self.__image_base_path, filename)
 
