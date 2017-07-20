5f4a237e8d29cfb3bea2b24320e8db948a55fd0e
Marc Duiker <marcduiker@gmail.com>
2017-07-20 11:48:46 +0200

    Allow different camera settings based on time of day.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import time
+import datetime
 from imagerecorder import ImageRecorder
 from camerasettings import CameraSettings
 from imageuploader import ImageUploader
@@ -10,12 +11,16 @@ current_frame = 1
 sleep_time = 5
 while current_frame < total_frames:
     camera_settings = CameraSettings()
-    # Change default camera settings here if required
-    camera_settings.isautomatic = False
-    camera_settings.exposuremode = 'off'
-    camera_settings.framerate = 6
-    camera_settings.iso = 800
-    camera_settings.shutterspeed = 4 # seconds
+    start_morning_hour = 6
+    start_evening_hour = 22
+    current_hour = datetime.datetime.now().hour 
+    if current_hour >= start_evening_hour or current_hour <= start_evening_hour:
+        # longer exposure and higher ISO for the night
+        camera_settings.isautomatic = False
+        camera_settings.exposuremode = 'off'
+        camera_settings.framerate = 6
+        camera_settings.iso = 800
+        camera_settings.shutterspeed = 4 # seconds
 
     image_path = ImageRecorder(camera_settings, image_base_path).capture_image()
     ImageUploader().store_image(image_path)
